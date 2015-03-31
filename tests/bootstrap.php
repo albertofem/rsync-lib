d61a791a255970dde500485eec067101285dcc44
James Brooks <james@bluebaytravel.co.uk>
2015-03-31 16:00:16 +0100

    Fix CS issues

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,37 +9,32 @@
  * the LICENSE file that was distributed with this source code.
  */
 
-$loader = require __DIR__ . '/../vendor/autoload.php';
+$loader = require __DIR__.'/../vendor/autoload.php';
 $loader->add('AFM\Rsync\Tests', __DIR__);
 
 /**
- * http://www.php.net/manual/en/function.rmdir.php#108113
+ * http://www.php.net/manual/en/function.rmdir.php#108113.
  */
 function rrmdir($dir)
 {
-	foreach(glob($dir . '/*') as $file)
-	{
-		if(is_dir($file))
-		{
-			rrmdir($file);
-		}
-		else
-		{
-			unlink($file);
-		}
-	}
+    foreach (glob($dir.'/*') as $file) {
+        if (is_dir($file)) {
+            rrmdir($file);
+        } else {
+            unlink($file);
+        }
+    }
 
-	rmdir($dir);
+    rmdir($dir);
 }
 
 function compare_directories($dir1, $dir2)
 {
-	$output = shell_exec("diff --brief " . $dir1 . " " . $dir2 . " 2>&1");
+    $output = shell_exec("diff --brief ".$dir1." ".$dir2." 2>&1");
 
-	if(strlen($output) > 0)
-	{
-		return false;
-	}
+    if (strlen($output) > 0) {
+        return false;
+    }
 
-	return true;
+    return true;
 }
